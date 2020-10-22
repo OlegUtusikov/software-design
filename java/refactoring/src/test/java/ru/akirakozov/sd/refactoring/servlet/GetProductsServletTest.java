9e53fcc461cd5763749e1fde87f1f29dce038445
Utusikov Oleg <o.utusikov@corp.vk.com>
2020-10-22 13:04:49 +0300

    Init tests

---
+++ java/refactoring/src/test/java/ru/akirakozov/sd/refactoring/servlet/GetProductsServletTest.java
@@ -0,0 +1,26 @@
+package ru.akirakozov.sd.refactoring.servlet;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GetProductsServletTest {
+
+    @Before
+    public void prepare() {
+
+    }
+
+    @After
+    public void finish() {
+
+    }
+
+    @Test
+    public void Test() {
+
+    }
+}
